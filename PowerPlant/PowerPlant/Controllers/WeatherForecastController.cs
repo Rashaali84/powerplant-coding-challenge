44530d925133a49c8a4dc7adb994c75fdba9170c
Rasha Ali <mohammad.metwally@gmail.com>
2023-06-04 21:52:18 +0200

    commit all last updates

--- PowerPlant/PowerPlant/Controllers/WeatherForecastController.cs
+++ PowerPlant/PowerPlant/Controllers/WeatherForecastController.cs
@@ -27,7 +27,9 @@ namespace PowerPlant.Controllers
             Fuels fuels = payload.fuels;
             List<PowerPlantItem> powerplants = payload.powerplants;
 
-            // Calculate the merit-order based on fuel costs
+            // Step 2: Calculate the merit-order based on fuel costs taking into account efficiency
+            //checking each power plant type and based on it we choos ethe correct fuel cost and multiply it with
+            //the 1 over the efficincy to know the cost per hour
             var sortedPowerPlants = powerplants.OrderBy(p =>
             {
                 double cost = 0;
@@ -35,42 +37,52 @@ namespace PowerPlant.Controllers
                     cost = fuels.gas * (1 / p.efficiency);
                 else if (p.type == "turbojet")
                     cost = fuels.kerosine * (1 / p.efficiency);
-                return cost;
+                return cost * p.pmin; // cost is also related to the pmin and the order will depend on both of that
             });
 
-            // Allocate power production for each power plant
+            // Step 3:Allocate power production for each power plant
             var powerProductions = new List<double>();
             foreach (var powerPlant in sortedPowerPlants)
             {
-                if (powerPlant.type == "windturbine")
+                if (load != 0)
                 {
-                    double windPower = (powerPlant.pmax * fuels.wind) / 100.0;
-                    powerProductions.Add(windPower);
-                    load -= (int)windPower;
+                    if (powerPlant.type == "windturbine")
+                    {
+                        double windPower = (powerPlant.pmax * fuels.wind) / 100.0;
+                        powerProductions.Add(windPower);
+                        load -= (int)windPower;
+                    }
+                    else if (powerPlant.type == "gasfired" || powerPlant.type == "turbojet")
+                    {
+                        int powerOutput = Math.Min(load, powerPlant.pmax);
+                        powerOutput = Math.Max(powerOutput, powerPlant.pmin);
+                        powerProductions.Add(powerOutput);
+                        load -= powerOutput;
+                    }
                 }
-                else if (powerPlant.type == "gasfired" || powerPlant.type == "turbojet")
-                {
-                    int powerOutput = Math.Min(load, powerPlant.pmax);
-                    powerOutput = Math.Max(powerOutput, powerPlant.pmin);
-                    powerProductions.Add(powerOutput);
-                    load -= powerOutput;
-                }
+                else
+                    powerProductions.Add(0);// in case the load is already distrbuted 
+
             }
 
             // Adjust the last power plant's production to match the load
-            if (load != 0)
+            //This is to test if there is a bug happened if there still load is not ditrbuted
+            //to be deleted
+            if (load < 0)
             {
                 int lastIndex = powerProductions.Count - 1;
                 powerProductions[lastIndex] += load;
+
+
             }
 
             // Round the power production values to the nearest multiple of 0.1 MW
             powerProductions = powerProductions.Select(p => Math.Round(p, 1)).ToList();
 
-            // Create the response JSON object
+            // Step 4:Create the response JSON object
             var response = new
             {
-                powerplants = powerplants.Select((p, index) => new
+                powerplants = sortedPowerPlants.Select((p, index) => new
                 {
                     name = p.name,
                     p = powerProductions[index]
@@ -79,5 +91,6 @@ namespace PowerPlant.Controllers
 
             return Ok(response);
         }
+
     }
 }
